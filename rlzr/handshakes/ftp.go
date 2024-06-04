f0ceb07ed4843766887f579db809be90a6bf822e
Wilmer S Zuna Largo <zunalargowilmer@gmail.com>
2024-06-03 21:09:11 -0700

    [handshake] Only amqp.

--- rlzr/handshakes/ftp.go
+++
@@ -1,8 +0,0 @@
-package handshakes
-
-import "rlzr/handshakes/ftp"
-
-func init() {
-	ftp.RegisterHandshake()
-}
-
--- rlzr/handshakes/telnet.go
+++
@@ -1,8 +0,0 @@
-package handshakes
-
-import "rlzr/handshakes/telnet"
-
-func init() {
-	telnet.RegisterHandshake()
-}
-
